6a2e0e89f6764db952e8012257353c12af724b6b
Igor de Lima Macêdo <igor.macedo097@gmail.com>
2024-05-17 17:09:46 -0300

    feat: API / CRUD em node para registro básico de docações

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+require('dotenv').config()
 
 const app = express();
 app.use(express.json());
 const port = 3000;
+const mongoUri = process.env.MONGO_URI;
+
 const Doacao = mongoose.model('Doacao', { 
     nome: String,
     descricao: String,
@@ -16,6 +18,11 @@ const Doacao = mongoose.model('Doacao', {
 app.get('/', async (req, res) => {
     const doacoes = await Doacao.find()
     return res.send(doacoes)
+})
+
+app.get('/:id', async (req, res) => {
+    const doacao = await Doacao.findById(req.params.id)
+    return res.send(doacao)
 })
 
 app.delete('/:id', async(req, res) => {
@@ -48,6 +55,6 @@ app.post('/', async(req, res) => {
 })
   
 app.listen(port, () => {
-    mongoose.connect('mongodb+srv://igormacedo097:3cLugBIaIXJvUUnX@node-api.fb0nqqm.mongodb.net/?retryWrites=true&w=majority&appName=node-api');
+    mongoose.connect(mongoUri);
     console.log(`Example app listening on port ${port}`)
 })
